5bedf8dda687c202e9c2a184d0a7687703ed8f57
Ruben Nuñez Gonzalez <rnunezgonzalez@danielcastelao.org>
2023-01-11 14:06:41 +0100

    D

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
       public static void main(String[] args) {
             System.out.println("Hola mundo!");
-            System.out.println("Adios");
+            System.out.println("Adios mundos");
       }
 }
